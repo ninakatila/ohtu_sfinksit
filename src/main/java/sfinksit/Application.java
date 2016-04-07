01361fe6034947664d844eb2db4b92ed71cc59fc
RonjaO <ronja.oja@gmail.com>
2016-04-07 20:49:52 +0300

    Spring-projektin kansiorakenne

---
+++ src/main/java/sfinksit/Application.java
@@ -0,0 +1,25 @@
+package sfinksit;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import javax.annotation.PostConstruct;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@SpringBootApplication
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+    
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
