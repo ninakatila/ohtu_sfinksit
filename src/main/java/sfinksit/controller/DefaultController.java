7550d08417d4ccb91a744cd52f675aaf8787369b
RonjaO <ronja.oja@gmail.com>
2016-04-07 21:48:34 +0300

    DefaultControlleriin pieni korjaus

--- src/main/java/sfinksit/controller/DefaultController.java
+++ src/main/java/sfinksit/controller/DefaultController.java
@@ -2,12 +2,14 @@ package sfinksit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("*")
 public class DefaultController {
 
-    public String hello() {
+    @RequestMapping(method=RequestMethod.GET)
+    public String view() {
         return "index";
     }
 }
