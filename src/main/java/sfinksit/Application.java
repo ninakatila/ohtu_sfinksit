9e9a6b7bfc18954464863a6b17aeab652c0aff6a
RonjaO <ronja.oja@gmail.com>
2016-04-09 15:03:39 +0300

    Korjasin typon Application.java:ssa

--- src/main/java/sfinksit/Application.java
+++ src/main/java/sfinksit/Application.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfigration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import javax.annotation.PostConstruct;
