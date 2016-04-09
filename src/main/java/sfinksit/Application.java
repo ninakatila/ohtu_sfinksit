8c7aa80a02922c8f157986db9bef221a18e88d36
RonjaO <ronja.oja@gmail.com>
2016-04-09 14:59:47 +0300

    EnableAutoConfiguration-annotaation kanssa Heroku ehkä jo toimii

--- src/main/java/sfinksit/Application.java
+++ src/main/java/sfinksit/Application.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfigration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import javax.annotation.PostConstruct;
@@ -14,6 +15,7 @@ import sfinksit.profiles.ProdProfile;
 
 @SpringBootApplication
 @Import({DevProfile.class, ProdProfile.class})
+@EnableAutoConfiguration
 public class Application {
 
     // private static final Logger log = LoggerFactory.getLogger(Application.class);
