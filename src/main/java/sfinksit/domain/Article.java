b4c0f184f90b5aab6f8a41352b9083179cd6bff1
RonjaO <ronja.oja@gmail.com>
2016-04-14 13:34:45 +0300

    Articleen importti...

--- src/main/java/sfinksit/domain/Article.java
+++ src/main/java/sfinksit/domain/Article.java
@@ -1,6 +1,7 @@
 package sfinksit.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
