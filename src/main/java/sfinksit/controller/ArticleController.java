9b739b5323f557f60262cf7c58bfb2acf9b1900f
Katila Nina M <kanikani@da5-cs-bk107-38.helsinki.fi>
2016-04-21 15:16:59 +0300

    Artikkelin sitaatiokeyn generointi

--- src/main/java/sfinksit/controller/ArticleController.java
+++ src/main/java/sfinksit/controller/ArticleController.java
@@ -20,35 +20,55 @@ import sfinksit.domain.Reference;
 @RequestMapping("/references/article")
 public class ArticleController {
 
+    private static char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+
     @Autowired
     private ReferenceRepository rep;
 
     @Autowired
     private ArticleRepository articlerep;
-    
-    @RequestMapping(method=RequestMethod.GET)
+
+    @RequestMapping(method = RequestMethod.GET)
     public String viewCreatePage(@ModelAttribute Reference reference) {
         return "article";
     }
-    
+
     @Transactional
-    @RequestMapping(method=RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     public String create(@Valid @ModelAttribute Reference reference, BindingResult bind, RedirectAttributes redirect, Model model) {
+
         if (bind.hasErrors()) {
             return "article";
         }
-        
+
+        if (reference.bibtexKey.isEmpty()) {
+
+            String key = reference.bibtexKey = reference.author.substring(0, 1) + reference.year;
+            if (rep.findExistingBibtexKey(key).size()>0) {
+                for (int i = 0; i < 6; i++) {
+                    key = key + alphabet[i];
+                    if (rep.findExistingBibtexKey(key).size()>0) {
+                        key = key.substring(0, key.length() - 1);
+                    } else {
+                        break;
+                    }
+
+                }
+            }
+            reference.bibtexKey = key;
+        }
+
         List list = rep.findExistingBibtexKey(reference.bibtexKey);
-        if (list.size() > 0){
+        if (list.size() > 0) {
             model.addAttribute("notvalidBibtexkey", "BibtexKey has to be unique");
             return "article";
         }
-        
+
         rep.save(reference);
-        
+
         redirect.addFlashAttribute("created", "Reference has created");
-        
+
         return "redirect:/";
     }
-    
+
 }
