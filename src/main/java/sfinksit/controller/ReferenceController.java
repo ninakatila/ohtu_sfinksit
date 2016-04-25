4fd9c0b5c32f2b850c7534e1ea5f1702ca88924c
Kössi <evkossi@gmail.com>
2016-04-25 21:03:06 +0300

    merkkijonon haku

--- src/main/java/sfinksit/controller/ReferenceController.java
+++ src/main/java/sfinksit/controller/ReferenceController.java
@@ -1,5 +1,6 @@
 package sfinksit.controller;
 
+import java.util.List;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import sfinksit.domain.Reference;
 import sfinksit.repository.ReferenceRepository;
 
@@ -39,6 +41,33 @@ public class ReferenceController {
     public String list(Model model) {
         model.addAttribute("references", rep.findAll());
         return "list";
+    }
+    
+    @RequestMapping(value="search", method=RequestMethod.GET)
+    public String viewfindThisObject(String name, @ModelAttribute Reference reference){
+        return "findObject";
+    }
+    
+    @RequestMapping(value="search", method=RequestMethod.POST)
+    public String findThisObject(@RequestParam(value = "search") String searchTerm, Model model){
+        List<Reference> list = rep.findSearchFromArticle(searchTerm);
+        List<Reference> list2 = rep.findSearchFromBook(searchTerm);
+        List<Reference> list3 = rep.findSearchFromConference(searchTerm);
+        list = combineLists(list, list2, list3);
+
+        model.addAttribute("references", list);
+
+        return "list";
+    }
+    
+    public List<Reference> combineLists(List<Reference> list, List<Reference> list2, List<Reference> list3) {
+        for (Reference ref : list2) {
+            list.add(ref);
+        }
+        for (Reference ref2 : list3) {
+            list.add(ref2);
+        }
+        return list;
     }
     
 }
